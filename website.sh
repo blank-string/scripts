90eb0d58269d80c380ceddd1b05d7dccad6a1474
Luke Preston <lukejpreston@gmail.com>
2018-04-20 06:24:02 +0100

    removing image issue

--- website.sh
+++ website.sh
@@ -6,15 +6,6 @@ npm whoami || npm login
 cd blankstring.com
 npm i -S @blank-string/data@latest @blank-string/rss@latest @blank-string/media@latest
 
-echo "Where is the new image located?"
-read image
-while [ ! -f $image ]
-do
-    echo "$image not found!"
-    echo "Where is the new image located?"
-    read image
-done
-cp $image ./public/static/media
 git add -A
 git commit -m 'next episode'
 git push
